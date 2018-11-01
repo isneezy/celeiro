840aeaf3dceb9390b7dae850d051ebecaca036e6
Ivan Vilanculo <vilanculoivan@gmail.com>
2018-11-01 17:49:38 +0200

    add lumen support

--- src/CeleiroServiceProvider.php
+++ src/CeleiroServiceProvider.php
@@ -3,31 +3,9 @@
 namespace Isneezy\Celeiro;
 
 
-use Illuminate\Container\Container;
-use Illuminate\Http\Request;
-use Illuminate\Support\ServiceProvider;
-use Isneezy\Celeiro\Contracts\Filterable as FilterableContract;
-use Isneezy\Celeiro\Filterable\Filterable;
-
 class CeleiroServiceProvider extends ServiceProvider {
-
 	public function boot() {
 		$resourcesDir = __DIR__ . '/../resources';
 		$this->publishes( [ "$resourcesDir/config/celeiro.php" => config_path( 'celeiro.php' ) ] );
-
-		$this->mergeConfigFrom( "$resourcesDir/config/celeiro.php", 'celeiro' );
-	}
-
-	public function register() {
-		$this->app->bind( FilterableContract::class, function () {
-			$request = Container::getInstance()->make( Request::class );
-
-			return Filterable::builder()
-			                 ->paged( ! $request->has( 'unpaged') )
-			                 ->page( $request->get( 'page', 1 ) )
-			                 ->limit($request->get('limit', config('celeiro.limit')) )
-			                 ->search( $request->get( 'q', '' ) )
-			                 ->toFilterable();
-		} );
 	}
 }
---
+++ src/ServiceProvider.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Isneezy\Celeiro;
+
+
+use Illuminate\Container\Container;
+use Illuminate\Http\Request;
+use Illuminate\Support\ServiceProvider as Service;
+use Isneezy\Celeiro\Contracts\Filterable as FilterableContract;
+use Isneezy\Celeiro\Filterable\Filterable;
+
+class ServiceProvider extends Service {
+
+	public function boot() {
+		$this->mergeConfigFrom( __DIR__.'/../resources/config/celeiro.php', 'celeiro');
+	}
+
+	public function register() {
+		$this->app->bind( FilterableContract::class, function () {
+			$request = Container::getInstance()->make( Request::class );
+
+			return Filterable::builder()
+			                 ->paged( ! $request->has( 'unpaged') )
+			                 ->page( $request->get( 'page', 1 ) )
+			                 ->limit($request->get('limit', config('celeiro.limit')) )
+			                 ->search( $request->get( 'q', '' ) )
+			                 ->toFilterable();
+		} );
+	}
+}
