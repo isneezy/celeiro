54f3f8a43456805e6077b11ce0f5ac2c0ba09eab
Ivan Vilanculo <vilanculoivan@gmail.com>
2018-11-13 14:54:15 +0200

    update scripts and comment out unecessary tests for a while

--- .travis/pgsql/test.sh
+++ .travis/pgsql/test.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 psql -c 'create database test;' -U postgres
-cp .travis/mysql/phpunit.xml ${DB}-phpunit.xml
+cp .travis/${DB}/phpunit.xml ${DB}-phpunit.xml
 phpunit --config ${DB}-phpunit.xml && \
     rm ${DB}-phpunit.xml
